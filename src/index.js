be11343b23485ea6f08072c20a0456ee73c5080b
Michael Rojas <michaelrojasp@gmail.com>
2020-10-11 12:16:03 -0600

    Replace JSON payload with TS Types example

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+
+// Replacing JSON characters to easily convert it into TS Model
+
+
+const replaseProps = /"(.+)":/
+const replaceStringVals = /:\s?"(.+)"/
+const replaceNumberVals = /:\s?(\d+(?:.\d+)?)/
+const replaseBooleanVals = /:\s?(true|false)/
+
